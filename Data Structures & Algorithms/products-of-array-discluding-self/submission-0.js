333a486d695e28accea7c9df9229310756ab0c1a
CodeRadmila <128687368+radm1la@users.noreply.github.com>
2026-04-05 15:32:45 +0400

    Add: products-of-array-discluding-self - submission-0

---
+++ Data Structures & Algorithms/products-of-array-discluding-self/submission-0.js
@@ -0,0 +1,22 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number[]}
+     */
+    productExceptSelf(nums) {
+        let res = [];
+        let leftPr = 1;
+        let rightPr = 1;
+        for (let i = 0; i < nums.length; i++) {
+            res[i] = leftPr;
+            leftPr *= nums[i];
+        }
+
+        for(let i = nums.length-1;i>=0;i--){
+            res[i] = res[i] * rightPr;
+            rightPr *= nums[i];
+        }
+
+        return res;
+    }
+}
